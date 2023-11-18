02a15227cd9f0389722980633d1cb30f598ee2fc
rfishe02 <fisher.renae@gmail.com>
2023-11-18 14:50:20 -0600

    Added task creation for database.

--- webapp/routes/tasks.js
+++ webapp/routes/tasks.js
@@ -15,14 +15,13 @@ router.get('/get', function(req, res, next) {
                     var task = {};
                     task["task_desc"] = row["task_desc"];
                     task["task_staus"] = row["task_status"];
-                    
+
                     data["tasks"].push(task);
                 });
                 data["status"] = "OK";
                 res.json(data);
             } else {
-                console.log(err); // TODO: Error logging
-                data["status"] = "ERROR";
+                console.log("TASK GET ERROR: "+err); // TODO: Error logging
                 return;
             }
         });
@@ -30,6 +29,35 @@ router.get('/get', function(req, res, next) {
 
 });
 
+router.post('/create', function(req, res, next) {
 
+    const userID = req.body.userID;
+    const taskDesc = req.body.taskDesc;
+
+    const db = new sqlite3.Database('./webapp.db');
+
+    db.serialize(() => {
+        const stmt = db.prepare(`insert into tasks (task_desc,task_status,task_user) values (?,0,?)`);
+        stmt.run(taskDesc,userID, (err) =>{
+            if(err === null) {
+                stmt.finalize();
+                res.send(`Created Task.`);
+            } else {
+                console.log("TASK REATE ERROR: "+err); // TODO: Error logging
+                return;
+            }
+        });
+        
+    });
+
+});
+
+router.patch('/finish', function(req, res, next) {
+    res.send(`Marked task as complete.`);
+});
+
+router.delete('/delete', function(req, res, next) {
+    res.send(`Removed task.`);
+});
 
 module.exports = router;
