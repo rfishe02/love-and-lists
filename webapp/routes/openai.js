ae9337f67fea2cd2489f356bd0aaae3a5b35a6d6
rfishe02 <fisher.renae@gmail.com>
2023-11-21 23:10:56 -0600

    Added prompts to openai.

--- webapp/routes/openai.js
+++ webapp/routes/openai.js
@@ -5,25 +5,44 @@ require('dotenv').config();
 const { OpenAI } = require('openai');
 const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
-router.get('/chat', async function(req, res, next) {
+function getSystem(charaName,charaType) {
+  return `You are a writer for an Otome game. You're writing about ${charaName}, a male character with a ${charaType} personality. Don't mention the word ${charaType}, but allude to it.`
+}
 
-  message = [{ role: "system", content: "You are a helpful AI assistant." }];
-  message.push({role: "user", content: "Hello."});
+function createChatPrompt(charaName,taskDesc) {
+  return `Write a short paragraph for a moment where ${charaName} offers praise for doing the activity: ${taskDesc}.`
+}
+
+function createImagePrompt(charHair,charEyes,charaType,taskDesc) {
+  return `anime style illustration, vibrant and detailed, male otome character with ${charHair} hair and ${charEyes} eyes, ${charaType} personality, context of ${taskDesc}, romantic atmosphere`;
+}
+
+router.get('/get', async function(req, res, next) {
+  const charaName = req.query.chara_name;
+  const charaHair = req.query.chara_hair;
+  const charaEyes = req.query.chara_eyes;
+  const charaType = req.query.chara_type;
+  const taskDesc = req.query.task_desc;
+
+  message = [{ role: "system", content: getSystem(charaName,charaType) }];
+  message.push({role: "user", content: createChatPrompt(charaName,taskDesc)});
 
   const chatCompletion = await openai.chat.completions.create({
-    messages: message,
+    messages: message, 
     model: "gpt-3.5-turbo",
   });
 
-  res.send(chatCompletion.choices[0]);
+  const image = await openai.images.generate({ 
+    prompt: createImagePrompt(charaHair,charaEyes,charaType,taskDesc),
+    model: "dall-e-3", 
+    n:1, 
+    quality:'standard',  
+    response_format: 'url',
+    size: '1024x1024',
+    style: 'vivid'
+  });
 
-});
-
-router.get('/image', async function(req, res, next) {
-
-  const image = await openai.images.generate({ model: "dall-e-3", prompt: "A cute baby sea otter" });
-  res.send(image.data);
-
+  res.json({"text":chatCompletion.choices[0].message.content,"image":image.data[0].url});
 });
 
 module.exports = router;
